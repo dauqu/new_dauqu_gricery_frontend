842fd181940b63b6b8eb878eb1034399e4d57a6f
Dauqu <95732637+dauqu@users.noreply.github.com>
2023-01-27 12:21:28 +0530

    update api url

--- src/components/Constant.js
+++ src/components/Constant.js
@@ -1,2 +1,4 @@
 // export const API = "https://tanveerbackend-production.up.railway.app";
-export const API = "https://tan.dauqu.host";
+// export const API = "https://tan.dauqu.host";
+export const API = "https://wordpress-dauqu-host-dwpnzm.dauqu.host/";
+
